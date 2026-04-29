d1a96b31e82ea0f6505a3f6f6e4f24aeb7dec142
ShklyaevPlaton <118743134+ShklyaevPlaton@users.noreply.github.com>
2026-04-29 20:54:28 +0300

    Update sort_test.cpp

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,9 +1,37 @@
 #include <gtest/gtest.h>
 
-// А вот тут collvalue.h не надо, берите просто инты
+#include <vector>
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
+TEST(SortTest, BubbleSortBasic) {
+    std::vector<int> v = {5, 3, 4, 1, 2};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({1,2,3,4,5}));
+}
+
+TEST(SortTest, QuickSortBasic) {
+    std::vector<int> v = {5, 3, 4, 1, 2};
+    quick_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({1,2,3,4,5}));
+}
+
+TEST(SortTest, EmptyArray) {
+    std::vector<int> v;
+    quick_sort(v.begin(), v.end());
+    EXPECT_TRUE(v.empty());
+}
+
+TEST(SortTest, SingleElement) {
+    std::vector<int> v = {42};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({42}));
+}
+
+TEST(SortTest, Duplicates) {
+    std::vector<int> v = {3, 1, 2, 1, 3};
+    quick_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({1,1,2,3,3}));
+}
 
 TEST(FooSuite, FooTest)
 {
