1c9704f3c291a55acadb5c74a1c2a0cb6b652d00
ShklyaevPlaton <118743134+ShklyaevPlaton@users.noreply.github.com>
2026-04-29 20:26:35 +0300

    Update sorting.h

--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -6,16 +6,48 @@
 // Для swap
 #include "collvalue.h"
 
-// ----------------------------- Пузырёк (например) --------------------------------------
+// ----------------------------- Пузырёк --------------------------------------
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+    if (begin == end) return;
+
+    for (Iterator i = begin; i != end; ++i) {
+        for (Iterator j = begin; j + 1 != end; ++j) {
+            if (*(j + 1) < *j) {
+                std::swap(*j, *(j + 1));
+            }
+        }
+    }
 }
 
-// --------------------------- QuickSort (например) ---------------------------------------
+// --------------------------- QuickSort ---------------------------------------
+template <typename Iterator>
+Iterator partition(Iterator begin, Iterator end)
+{
+    auto pivot = *(end - 1);
+    Iterator i = begin;
+
+    for (Iterator j = begin; j != end - 1; ++j) {
+        if (*j < pivot) {
+            std::swap(*i, *j);
+            ++i;
+        }
+    }
+
+    std::swap(*i, *(end - 1));
+    return i;
+}
+
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+    if (end - begin <= 1) return;
+
+    Iterator pivot = partition(begin, end);
+
+    quick_sort(begin, pivot);
+    quick_sort(pivot + 1, end);
 }
 
 #endif // SORTING_H
