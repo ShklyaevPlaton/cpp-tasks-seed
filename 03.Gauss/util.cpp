80fa5978a7d466333ef214609e1ae3a4d8ae4603
ShklyaevPlaton <118743134+ShklyaevPlaton@users.noreply.github.com>
2026-06-08 17:26:43 +0300

    Refactor load_csv_to_matrix and print_matrix_as_csv

--- 03.Gauss/util.cpp
+++ 03.Gauss/util.cpp
@@ -7,33 +7,40 @@
 
 #include "lazycsv.hpp"
 
-GaussMatrix load_csv_to_matrix(const char *filename) {
+GaussMatrix load_csv_to_matrix(const char *filename)
+{
     std::vector<std::vector<double>> values;
 
-    lazycsv::parser parser{filename};
+    lazycsv::parser parser {filename};
 
-    for (const auto row : parser) {
+    for (const auto row : parser)
+    {
         std::vector<double> current_row;
 
-        for (const auto cell : row) {
+        for (const auto cell : row)
+        {
             current_row.push_back(std::stod(std::string(cell.raw())));
         }
 
-        if (!values.empty() && current_row.size() != values.front().size()) {
+        if (!values.empty() && current_row.size() != values.front().size())
+        {
             throw std::runtime_error("CSV rows have different sizes");
         }
 
         values.push_back(current_row);
     }
 
-    if (values.empty()) {
+    if (values.empty())
+    {
         throw std::runtime_error("CSV file is empty");
     }
 
     GaussMatrix matrix(values.size(), values.front().size());
 
-    for (int i = 0; i < static_cast<int>(values.size()); ++i) {
-        for (int j = 0; j < static_cast<int>(values[i].size()); ++j) {
+    for (int i = 0; i < static_cast<int>(values.size()); ++i)
+    {
+        for (int j = 0; j < static_cast<int>(values[i].size()); ++j)
+        {
             matrix(i, j) = values[i][j];
         }
     }
@@ -41,14 +48,18 @@ GaussMatrix load_csv_to_matrix(const char *filename) {
     return matrix;
 }
 
-void print_matrix_as_csv(std::ostream &out, const GaussMatrix &matrix, int prec) {
+void print_matrix_as_csv(std::ostream &out, const GaussMatrix &matrix, int prec)
+{
     out << std::fixed << std::setprecision(prec);
 
-    for (int i = 0; i < matrix.rows(); ++i) {
-        for (int j = 0; j < matrix.cols(); ++j) {
+    for (int i = 0; i < matrix.rows(); ++i)
+    {
+        for (int j = 0; j < matrix.cols(); ++j)
+        {
             out << matrix(i, j);
 
-            if (j + 1 < matrix.cols()) {
+            if (j + 1 < matrix.cols())
+            {
                 out << ',';
             }
         }
