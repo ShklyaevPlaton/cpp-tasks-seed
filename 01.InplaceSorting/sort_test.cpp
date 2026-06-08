2e6e9fb990d8b4636b64504523edd894d28f9088
ShklyaevPlaton <118743134+ShklyaevPlaton@users.noreply.github.com>
2026-06-08 17:19:11 +0300

    Update sort_test.cpp

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,39 +1,50 @@
 #include <gtest/gtest.h>
 
 #include <vector>
+
 #include "sorting.h"
 
-TEST(SortTest, BubbleSortBasic) {
+TEST(SortTest, BubbleSortBasic)
+{
     std::vector<int> v = {5, 3, 4, 1, 2};
+
     bubble_sort(v.begin(), v.end());
-    EXPECT_EQ(v, std::vector<int>({1,2,3,4,5}));
+
+    EXPECT_EQ(v, std::vector<int>({1, 2, 3, 4, 5}));
 }
 
-TEST(SortTest, QuickSortBasic) {
+TEST(SortTest, QuickSortBasic)
+{
     std::vector<int> v = {5, 3, 4, 1, 2};
+
     quick_sort(v.begin(), v.end());
-    EXPECT_EQ(v, std::vector<int>({1,2,3,4,5}));
+
+    EXPECT_EQ(v, std::vector<int>({1, 2, 3, 4, 5}));
 }
 
-TEST(SortTest, EmptyArray) {
+TEST(SortTest, EmptyArray)
+{
     std::vector<int> v;
+
     quick_sort(v.begin(), v.end());
+
     EXPECT_TRUE(v.empty());
 }
 
-TEST(SortTest, SingleElement) {
+TEST(SortTest, SingleElement)
+{
     std::vector<int> v = {42};
+
     bubble_sort(v.begin(), v.end());
+
     EXPECT_EQ(v, std::vector<int>({42}));
 }
 
-TEST(SortTest, Duplicates) {
-    std::vector<int> v = {3, 1, 2, 1, 3};
-    quick_sort(v.begin(), v.end());
-    EXPECT_EQ(v, std::vector<int>({1,1,2,3,3}));
-}
-
-TEST(FooSuite, FooTest)
+TEST(SortTest, Duplicates)
 {
-    EXPECT_EQ(0, 0);
+    std::vector<int> v = {3, 1, 2, 1, 3};
+
+    quick_sort(v.begin(), v.end());
+
+    EXPECT_EQ(v, std::vector<int>({1, 1, 2, 3, 3}));
 }
