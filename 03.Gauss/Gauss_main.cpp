3b53e8726dbb0095fcbeb1c9014e0a86b1d5b2b9
ShklyaevPlaton <118743134+ShklyaevPlaton@users.noreply.github.com>
2026-06-08 17:27:03 +0300

    Refactor main function for improved readability

--- 03.Gauss/Gauss_main.cpp
+++ 03.Gauss/Gauss_main.cpp
@@ -4,26 +4,33 @@
 #include "Gauss_solve.h"
 #include "util.h"
 
-int main(int argc, const char *argv[]) {
-    if (argc != 2) {
+int main(int argc, const char *argv[])
+{
+    if (argc != 2)
+    {
         std::cerr << "Usage: " << argv[0] << " AB.csv\n";
         return 1;
     }
 
-    try {
+    try
+    {
         GaussMatrix ab = load_csv_to_matrix(argv[1]);
         GaussVector solution = Gauss_solve(ab);
 
-        for (int i = 0; i < solution.size(); ++i) {
+        for (int i = 0; i < solution.size(); ++i)
+        {
             std::cout << solution(i);
 
-            if (i + 1 < solution.size()) {
+            if (i + 1 < solution.size())
+            {
                 std::cout << ',';
             }
         }
 
         std::cout << '\n';
-    } catch (const std::exception &error) {
+    }
+    catch (const std::exception &error)
+    {
         std::cerr << "ERROR: " << error.what() << '\n';
         return 1;
     }
